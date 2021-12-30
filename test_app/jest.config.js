0c92630a380c4185c0e29cca72adda1036d3a900
Adam Catalfano <adam.catalfano@rethinkfirst.com>
2021-12-30 13:21:40 -0500

    it works! (until you try to add an import from lodash-es!)

--- test_app/jest.config.js
+++ test_app/jest.config.js
@@ -18,13 +18,22 @@ module.exports = {
     },
     moduleDirectories: ['node_modules', 'src'],
     moduleFileExtensions: [
+        // TODO: include 'html' and it sees "TestBed" as undefined
+        //          exclude and it can't import from lodash-es b/c "unexpected token 'export'"
+        //                                          ^^ from inside lodash-es/lodash.js at export { default as add } from './add.js'
         // 'html',
         'js',
         'json',
         'ts',
-        'mjs'
+        // 'mjs'
     ],
-    modulePathIgnorePatterns: ['<rootDir>/node_modules/.*'],
+    moduleNameMapper: {
+        '^src/(.*)$': '<rootDir>/src/$1',
+        '^app/(.*)$': '<rootDir>/src/app/$1',
+        '^assets/(.*)$': '<rootDir>/src/assets/$1',
+        '^environments/(.*)$': '<rootDir>/src/environments/$1',
+    },
+    // modulePathIgnorePatterns: ['<rootDir>/node_modules/.*'],
     preset: 'jest-preset-angular',
     roots: ['src'],
     setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
@@ -39,7 +48,10 @@ module.exports = {
         // '^.+\\.(ts|html)$': 'ts-jest',
         //'().+\\.ts$': 'babel-jest'
         // '^.+\\.js$': 'babel-jest',
+        '^.+\\.(ts|js|html)$': 'jest-preset-angular',
     },
     // transformIgnorePatterns: ['/node_modules/(?!.*\\.mjs$)']
     // transformIgnorePatterns: ['/node_modules/(?!@ngrx)']
+    transformIgnorePatterns: ['/node_modules/(?!lodash-es)']
+    // include this ^^^^^ (or the ngrx one) and get "cannot use import statement outside a module"
 };
--- test_app/src/app/child/child.component.ts
+++ test_app/src/app/child/child.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { isNil as _isNil } from 'lodash-es';
 
 @Component({
   selector: 'tst-child',
   templateUrl: './child.component.html',
-  styleUrls: ['./child.component.scss']
+  styleUrls: ['./child.component.scss'],
 })
 export class ChildComponent implements OnInit {
+  public val!: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    if (_isNil(this.val)) {
+      this.val = 'default value';
+    }
   }
-
 }

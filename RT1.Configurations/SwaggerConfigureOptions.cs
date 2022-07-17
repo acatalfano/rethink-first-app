0902e5f9721131c5e8fdb638297ba2a7d59925c8
Adam Catalfano <acatalfano@gmail.com>
2022-07-16 22:40:14 -0400

    cleanup

--- RT1.Configurations/SwaggerConfigureOptions.cs
+++ RT1.Configurations/SwaggerConfigureOptions.cs
@@ -10,17 +10,21 @@ namespace RT1.Configurations
     {
         private readonly IApiVersionDescriptionProvider provider;
 
-        public SwaggerConfigureOptions(IApiVersionDescriptionProvider provider) => this.provider = provider;
+        public SwaggerConfigureOptions(IApiVersionDescriptionProvider provider)
+            => this.provider = provider;
 
         public void Configure(SwaggerGenOptions options)
         {
             foreach (var description in provider.ApiVersionDescriptions)
             {
-                options.SwaggerDoc(description.GroupName, new Microsoft.OpenApi.Models.OpenApiInfo
-                {
-                    Title = "Rethink First App API",
-                    Version = description.ApiVersion.ToString()
-                });
+                options.SwaggerDoc(
+                    description.GroupName,
+                    new Microsoft.OpenApi.Models.OpenApiInfo
+                    {
+                        Title = "Rethink First App API",
+                        Version = description.ApiVersion.ToString()
+                    }
+                );
             }
         }
     }
